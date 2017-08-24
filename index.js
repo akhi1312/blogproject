d82e47e0d119eb864219c3d6d9c28db2dd0a359a
Akhilesh <akhilesh.deowanshi@hotmail.com>
2017-08-24 15:45:01 -0700

    Server Setup

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const config = require('./config/database');
+const path  = require('path');
+
+
+
+
+
+mongoose.connect(config.uri ,(err) => {
+    if(err) {
+        console.log("could not connect to database" , err);
+    }
+    else {
+        
+        console.log("Connected to Database " + config.db) ;
+    }
+});
+
+
+app.use(express.static(__dirname + '/client/dist/'));
+
+
+
+
+app.get('*', function(req, res){
+  res.sendFile(path.join(__dirname + '/client/dist/index.html'));
+});
+
+app.listen(8080,() =>{
+    console.log ("Listening on port 8008");
+})  ; 
